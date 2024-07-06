912f73b0d0891ac272721c60ae2da43dbf06778b
Aly Raffauf <aly@raffauflabs.com>
2024-07-05 20:12:49 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Nix modules for Raffauflabs.com";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+  outputs = inputs @ {self, ...}: {
+    formatter = inputs.nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ] (system: inputs.nixpkgs.legacyPackages.${system}.alejandra);
+
+    nixosModules.raffauflabs =
+      import ./raffauflabsModules inputs;
+
+    nixosModules.default = self.nixosModules.raffauflabs;
+  };
+}
